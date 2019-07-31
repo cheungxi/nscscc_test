5bc09ac866e46efea7886f388b105020d3246d55
Zhang Xi <zhangxi@DESKTOP-R1S5DLE.localdomain>
2019-07-31 23:30:25 +0800

    debug device

--- axi_perf_test/program_device.tcl
+++ axi_perf_test/program_device.tcl
@@ -15,7 +15,7 @@ foreach team [glob -types d -directory $Team_Submission_Dir/ -tail *] {
     if {$var == {1}} {
     set_property PROBES.FILE {} [get_hw_devices xc7a200t_0]
     set_property FULL_PROBES.FILE {} [get_hw_devices xc7a200t_0]
-    set_property PROGRAM.FILE "$Result_Dir/$team/$Test_Item/soc_axi_lite_top.bit" [get_hw_devices xc8a200t_0]
+    set_property PROGRAM.FILE "$Result_Dir/$team/$Test_Item/soc_axi_lite_top.bit" [get_hw_devices xc7a200t_0]
     program_hw_devices [get_hw_devices xc7a200t_0]
     refresh_hw_device [lindex [get_hw_devices xc7a200t_0] 0]
     puts "$team Program Devices Successfully !"
