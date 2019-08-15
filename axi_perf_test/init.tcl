a4dc8494935b778d8b6c214ac422ec970a804021
Zhang Xi <zhangxi1996@vip.qq.com>
2019-08-16 02:39:16 +0800

    improve program device & final test

--- axi_perf_test/init.tcl
+++ axi_perf_test/init.tcl
@@ -32,7 +32,7 @@ proc findFiles { basedir pattern } {
 
 set Team_Submission_Dir ../team_submission
 set Test_Project_Dir ./test_project
-set Source_Select soc_axi_perf
+set Source_Select performance/soc_axi_perf
 
 foreach team [glob -types d -directory $Team_Submission_Dir/ -tail *] {
     #copy init project
